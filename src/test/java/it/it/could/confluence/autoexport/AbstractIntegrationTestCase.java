aadaff231f1e4d10de3dc6ce9f5dfcea8ad6f6a0
donald.brown <donald.brown@0af91f53-0d40-0410-869e-9519fa2e6b12>
2010-04-28 07:15:11 +0000

    Upgrading to build and test against 3.2.1

--- src/test/java/it/it/could/confluence/autoexport/AbstractIntegrationTestCase.java
+++ src/test/java/it/it/could/confluence/autoexport/AbstractIntegrationTestCase.java
@@ -2,23 +2,33 @@ package it.it.could.confluence.autoexport;
 
 import com.atlassian.confluence.plugin.functest.AbstractConfluencePluginWebTestCase;
 import com.atlassian.confluence.plugin.functest.ConfluenceWebTester;
+import com.atlassian.confluence.plugin.functest.JWebUnitConfluenceWebTester;
+import com.atlassian.confluence.plugin.functest.TesterConfiguration;
+import junit.framework.Assert;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class AbstractIntegrationTestCase extends AbstractConfluencePluginWebTestCase
 {
-    @Override
-    protected ConfluenceWebTester createConfluenceWebTester()
-    {
-        ConfluenceWebTester tester = new ConfluenceWebTester();
-        tester.setAdminUserName("admin");
-        tester.setAdminPassword("admin");
-        tester.setHostName("localhost");
-        tester.setPort(Integer.parseInt(System.getProperty("http.port", "1990")));
-        tester.setContextPath(System.getProperty("context.path", "/confluence"));
-        tester.setProtocol("http");
-        tester.setSoapServicePath("/rpc/soap-axis/confluenceservice-v1?wsdl");
-        tester.getTestContext().setBaseUrl(tester.getBaseUrl());
-        tester.setScriptingEnabled(false);
 
-        return tester;
+    @Override
+    protected JWebUnitConfluenceWebTester createConfluenceWebTester()
+    {
+        try
+        {
+            Properties props = new Properties();
+            props.setProperty("confluence.webapp.port", System.getProperty("http.port", "1990"));
+            props.setProperty("confluence.webapp.context.path", System.getProperty("context.path", "/confluence"));
+            return new JWebUnitConfluenceWebTester(new TesterConfiguration(props));
+        }
+        catch (IOException ioe)
+        {
+            Assert.fail("Unable to create tester: " + ioe.getMessage());
+            return null;
+        }
+        //tester.getTestContext().setBaseUrl(tester.getBaseUrl());
+        //tester.setScriptingEnabled(false);
+
     }
 }
